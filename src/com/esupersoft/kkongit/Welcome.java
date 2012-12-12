10a7e37b2826c9597e236ae87e194f1c9a70ec30
Kailash Karigar <karigarkc@gmail.com>
2012-12-12 05:10:28 -0800

    1. My first commit to git

---
+++ src/com/esupersoft/kkongit/Welcome.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.esupersoft.kkongit;
+
+/**
+ * @author Karigar
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
